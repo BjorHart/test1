15211a9ed6e9ba1a267352960163bbaaa476a1cd
Paal A.S. Thorseth <paalthorseth@gmail.com>
2019-07-23 09:10:50 +0200

    Changed src.ino to test readPotAngle

--- Arduino/src/src.ino
+++ Arduino/src/src.ino
@@ -13,12 +13,8 @@ void setup(){
 }
 
 void loop(){
-  /*
-    readPotAngle(A0);
-    delay(1000);
-  */
-   int sensorValue = analogRead(A0); // read input from potPin
-    Serial.print("sensorValue: "); Serial.println(sensorValue);
 
+  readPotAngle(A0;
+  delay(1000);
 
 }
