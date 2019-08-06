21c25d838490b742eb865077c52fa25fb0832ea5
Martin Lund Haug <martinlhaug@gmail.com>
2019-08-06 16:25:16 +0200

    Updated transmitSensorData

--- Arduino/libraries/SensorFusion/transmitSensorData/transmitSensorData.ino
+++ Arduino/libraries/SensorFusion/transmitSensorData/transmitSensorData.ino
@@ -3,7 +3,7 @@
 
 
 void setup() {
-  Serial.println("HEI");
+  Serial.println("SETUP");
   weightSensorSetup();
   rotationSensorSetup();
   MQTTSetup();
@@ -11,22 +11,9 @@ void setup() {
 }
 
 void loop() {
-  /*
-  Serial.print(potSensorUpdate(0));
-  Serial.print(": ");
-  Serial.print(potSensorUpdate(1));
-  Serial.print(": ");
-  Serial.print(potSensorUpdate(2));
-  Serial.print(": ");
-  Serial.print(weightSensorUpdate());
-  Serial.print(": ");
-  Serial.print(rotSensorUpdate());
-
-*/
+ 
  wdt_reset();
  int a = rotSensorUpdate();
  MQTTSend(potSensorUpdate(0), potSensorUpdate(1), potSensorUpdate(2), weightSensorUpdate(), a);
- while(1) {
-  //
- }
+
 }
