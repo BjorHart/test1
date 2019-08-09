4761dacfe46d57ff6ddd4f30f113c361cfe7fe37
Martin Lund Haug <martlh@stud.ntnu.no>
2019-08-09 10:26:59 +0200

    Added zip version of vuforia project

--- Vuforia/Projects/Robot_arm_0908/src/phone/components/Explore.js
+++
@@ -1,7 +0,0 @@
-// $scope, $element, $attrs, $injector, $sce, $timeout, $http, $ionicPopup, and $ionicPopover services are available
-
-
-// function to redirect to edr home page
-$scope.goToEDR = function () {
-	window.location.href = 'https://edrmedeso.com/'	  
-};
