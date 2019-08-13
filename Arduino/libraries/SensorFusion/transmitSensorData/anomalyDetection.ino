75d1f24e6f20ed9ad7573acd1ccc923603b0a604
Martin Lund Haug <martlh@stud.ntnu.no>
2019-08-13 16:14:02 +0200

    Added calibration routine to the 6axis sensor. Not working properly yet

--- Arduino/libraries/SensorFusion/transmitSensorData/anomalyDetection.ino
+++ Arduino/libraries/SensorFusion/transmitSensorData/anomalyDetection.ino
@@ -1,8 +1,8 @@
 
-
+/*
 int anomalyDetect() {
 
-  if (abs(robotArmPos - sensorValue) > 20) {
+  if (abs(robotArmPos(1, true) - sensorValue(1)) > 20) {
     // Difference between desired robot position and sensor value
     // is(probably) larger than 20 degrees.
     // Do something
@@ -10,12 +10,25 @@ int anomalyDetect() {
   
 }
 
+int robotArmPos(int servo, bool functionFinished) {
 
-int robotArmPos(int part) {
+ // When movement function is over 
+ // return input value on function
+
+ // If (functionFinished) {
+ //   return servo
+ // }
+ //
   return 0;
 }
-
 
 int sensorValue(int sensor) {
   return 0;
 }
+
+// Need to get signal from Braccio when movement function is finished, then
+// compare input value to the function with the sensor value. If difference maybe signal it 
+// on sensor or servo
+// Alternative is to send angle from Braccio for every degree increment and compare it. 
+
+*/
--- Arduino/libraries/SensorFusion/transmitSensorData/transmitSensorData.ino
+++ Arduino/libraries/SensorFusion/transmitSensorData/transmitSensorData.ino
@@ -3,27 +3,28 @@
 
 
 void setup() {
-  Serial.begin(115200); // May be removed
-  
+  Serial.begin(9600); // May be removed
+
+  // When restarting, maybe send data to robot arm to start initialization over again
+  Serial.println("Setup");
   //Watchdog routing
   MCUSR = 0;
   wdt_disable();
   digitalWrite(13, 1); // LED ON indicating setup procedure
   delay(1000);
-  //Serial.println("SETUP");M
-  wdt_enable(WDTO_1S);
+  wdt_enable(WDTO_4S);
   
   //weightSensorSetup();
   rotationSensorSetup();
-  //MQTTSetup();
+ // MQTTSetup();
   
 }
 
 void loop() {
 
- digitalWrite(13, 0);
- int a = rotSensorUpdate();
-// MQTTSend(potSensorUpdate(0), potSensorUpdate(1), potSensorUpdate(2), weightSensorUpdate(), a);
+ digitalWrite(13, 0); // LED OFF indicating in loop
+ int a = rotationSensorUpdate();
+ //MQTTSend(potSensorUpdate(0), potSensorUpdate(1), potSensorUpdate(2), weightSensorUpdate(), a);
  Serial.println(a);
  wdt_reset(); // If sending takes more than 1 second, the program is restarted, indicating a crash most likely in the 6-axis sensor
 }
