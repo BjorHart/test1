78b99211fd5813bd0e7221dc04932217689cf896
Paal Thorseth <pathorse@stud.ntnu.no>
2019-09-08 19:34:45 +0200

    Added run.bat file

---
+++ run.bat
@@ -0,0 +1,6 @@
+rem note: This file should be run on the Virtual Machine in order to start the Vuforia Experience Server and Twin Runtime
+
+@echo off
+    start "Vuforia Experience Server" \B "C:\ptc\bin\start-es.exe" rem dobbelsjekk denne
+    rem start "Twin Runtime" \B LEGG INN RIKTIG PATH FOR Å STARTE RUNTIME
+@echo on
