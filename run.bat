6de0c66ac87b6ba7c09c4dc5a1d6206e3b93a30d
BjornHartmann <48647622+BjornHartmann@users.noreply.github.com>
2019-09-09 13:02:17 +0200

    Updater run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
-﻿rem note: This file should be run on the Virtual Machine in order to start the Vuforia Experience Server and Twin Runtime
-
-@echo off
-    start "Vuforia Experience Server" \B "C:\ptc\studio-es\bin\start-es.exe" rem dobbelsjekk denne
-    "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" 
-@echo on
+rem note: This file should be run on the Virtual Machine in order to start the Vuforia Experience Server and Twin Runtime
+"C:\ptc\bin\start-es.exe" 
+start "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" "C:\Users\ansys\Desktop\SummerInterns\Git\Summer-Intern-Project-2019\RuntimePythonForTwin\runCleaned\RuntimeFiles\BraccioRuntime\application.py"
+start "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" "C:\Users\ansys\Desktop\SummerInterns\Git\Summer-Intern-Project-2019\RuntimePythonForTwin\runCleaned\RuntimeFiles\BraccioRuntime\accumulatedDamage.py"
