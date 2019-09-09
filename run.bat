29adfd7f049a822705783f7ae26cc44dd585ab5b
Bjørn Hartmann <bjorn.hartmann@hotmail.no>
2019-09-09 10:45:10 +0200

    changed .bat file

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-rem note: This file should be run on the Virtual Machine in order to start the Vuforia Experience Server and Twin Runtime
+﻿rem note: This file should be run on the Virtual Machine in order to start the Vuforia Experience Server and Twin Runtime
 
 @echo off
-    start "Vuforia Experience Server" \B "C:\ptc\bin\start-es.exe" rem dobbelsjekk denne
-    rem start "Twin Runtime" \B LEGG INN RIKTIG PATH FOR Å STARTE RUNTIME
+    start "Vuforia Experience Server" \B "C:\ptc\studio-es\bin\start-es.exe" rem dobbelsjekk denne
+    "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" 
 @echo on
